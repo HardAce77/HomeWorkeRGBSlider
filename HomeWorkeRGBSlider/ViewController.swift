a5e861d8982ba304cf15906091f1ae9d5802f1ce
Nikita <nelub77@icloud.com>
2022-09-25 23:28:36 +0500

    Initial Commit

---
+++ HomeWorkeRGBSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkeRGBSlider
+//
+//  Created by Nikita Neluybin on 25.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
